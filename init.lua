d96ba7434ef308a11b2916a1b778172bd8ae8989
Ironmanhim3 <a.resende.victor@gmail.com>
2024-03-02 10:48:21 +0200

    Reorganized file format

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Hydrogen block
+minetest.register_node("periodic_elements:hydrogen_block", {
+    description = "did you know that hydrogen can go big boom",
+    tiles = {"hydrogen_block.png"},
+    groups = {cracky = 3, stone = 1},
+})
+-- Helium block
+minetest.register_node("periodic_elements:helium_block", {
+    description = "helium is the second most abundant element in the UNIVERSE!",
+    tiles = {"helium_block.png"},
+    groups = {cracky = 3, stone = 1},
+})
