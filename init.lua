166ad3d7e30612e854f51bdaa4fb308e47a760df
Ironmanhim3 <a.resende.victor@gmail.com>
2024-03-06 17:01:03 +0200

    Fixed some bugs

--- init.lua
+++ init.lua
@@ -1,48 +1,48 @@
 -- Hydrogen block
 minetest.register_node("periodic_elements:hydrogen_block", {
     description = "Hydrogen block",
-    tiles = {"hydrogen_block.png"},
+    tiles = {"hydrogen.png"},
     groups = {cracky = 3, stone = 1},
 })
 -- Helium block
 minetest.register_node("periodic_elements:helium_block", {
     description = "Helium block",
-    tiles = {"helium_block.png"},
+    tiles = {"helium.png"},
     groups = {cracky = 3, stone = 1},
 })
 -- Lithium block
 minetest.register_node("periodic_elements:lithium_block", {
     description = "Lithium block",
-    tiles = {"lithium_block.png"},
+    tiles = {"lithium.png"},
     groups = {cracky = 3, stone = 1},
 })
 -- Beryllium block
 minetest.register_node("periodic_elements:beryllium_block", {
     description = "Beryllium block",
-    tiles = {"beryllium_block.png"},
+    tiles = {"beryllium.png"},
     groups = {cracky = 3, stone = 1},
 })
 -- Boron block
 minetest.register_node("periodic_elements:boron_block", {
     description = "Boron block",
-    tiles = {"boron_block.png"},
+    tiles = {"boron.png"},
     groups = {cracky = 3, stone = 1},
 })
 -- Carbon block
 minetest.register_node("periodic_elements:carbon_block", {
     description = "Carbon block",
-    tiles = {"carbon_block.png"},
+    tiles = {"carbon.png"},
     groups = {cracky = 3, stone = 1},
 })
 -- Nitrogen block
 minetest.register_node("periodic_elements:nitrogen_block", {
     description = "Nitrogen block",
-    tiles = {"nitrogen_block.png"},
+    tiles = {"nitrogen.png"},
     groups = {cracky = 3, stone = 1},
 })
 -- Oxygen block
 minetest.register_node("periodic_elements:oxygen_block", {
     description = "Oxygen block",
-    tiles = {"oxygen_block.png"},
+    tiles = {"oxygen.png"},
     groups = {cracky = 3, stone = 1},
 })
